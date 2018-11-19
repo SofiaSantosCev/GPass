885edbeb1529f45a0b9d8a0e9b40f93776dc332a
alumnos <sofia_santos_apps1ma1718@cev.com>
2018-11-19 13:05:17 +0100

    migraciones creadas

--- database/migrations/2018_11_19_115450_create_categories_table.php
+++ database/migrations/2018_11_19_115450_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSongsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateSongsTable extends Migration
      */
     public function up()
     {
-        Schema::create('songs', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
+            $table->string('name',50);
+            $table->integer('id_user');
         });
     }
 
@@ -26,6 +27,6 @@ class CreateSongsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('songs');
+        Schema::dropIfExists('categories');
     }
 }
--- database/migrations/2018_11_19_115523_create_passwords_table.php
+++ database/migrations/2018_11_19_115523_create_passwords_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreatePasswordsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('passwords', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',50);
+            $table->string('password',20);
+            $table->integer('id_category');
+            $table->integer('id_user');
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('passwords');
     }
 }
