6d1bd96b190e96217212779f744ad36c5b8fd50f
alumnos <sofia_santos_apps1ma1718@cev.com>
2018-12-13 12:43:11 +0100

    agregados archivos cliente web

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,190 +1,201 @@
 <?php
+        namespace App\Http\Controllers;
 
-namespace App\Http\Controllers;
+        use App\Category;
+        use Illuminate\Http\Request;
+        use App\User;
+        use App\Http\Controllers\LoginController;
 
-use App\Category;
-use Illuminate\Http\Request;
-use App\User;
-use App\Http\Controllers\LoginController;
-
-class CategoryController extends Controller
-{
-
-    private function IsLoggedIn()
-    {
-        $headers = getallheaders();
-        if (!isset($headers['Authorization'])) 
+        class CategoryController extends Controller
         {
-            return false;
 
-        } else {
-            
-            $user = self::getUserfromToken();
-            $tokenDecoded = LoginController::decodeToken($headers['Authorization']);
-            if ($tokenDecoded->password == $user->password and $tokenDecoded->email == $user->email) 
+            private function IsLoggedIn()
             {
-                return true;
-            }
-        }
-    }
+                $headers = getallheaders();
+                if (!isset($headers['Authorization'])) 
+                {
+                    return false;
 
-    private function getUserfromToken(){
-        $headers = getallheaders();
-        $tokenDecoded = LoginController::decodeToken($headers['Authorization']);
-        $user = User::where('email', $tokenDecoded->email)->first();
-        return $user;
-    }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index() //shows all categories 
-    {
-        if (self::isLoggedIn())
-        {   
-            $categories = Category::all();
-            $categoryNames = [];
+                } else {
 
-            if(empty($categories))
-            {
-                return response("There are no categories created",400);
-            } 
-                foreach ($categories as $category) {
-                    array_push($categoryNames, $category->name);
-                } 
-                return response()->json([
-                    'categories' => $categoryNames;
-                ]);
-            
-
-        } else {
-            return response("You don't have permission", 403);
-        }
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        if (self::isLoggedIn())
-        {
-            $name = $_POST['name'];
-
-            if (!ctype_graph($name)) {
-                return response("The name of the category can't have any blank space", 400); exit;
-            }
-
-            if (empty($name)) {
-                return response("The name of the category is empty", 400); exit;
-            }
-
-            $category = Category::where('name', $name)->first();
-
-            if ($category != null) {
-                if ($name != $category->name) {
-                    return response("This category already exists",400); exit;
-                }
-            }
-            
-
-            $user_id = self::getUserfromToken()->id; //id del usuario del token;
-
-            $category = new Category;
-
-            $category->name = $name;
-            $category->user_id = $user_id;
-
-            $category->save();
-
-        } else {
-            return response("You don't have permission", 403);
-        }
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
-    {
-        
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function update($id, Request $request)
-    {
-        if (self::isLoggedIn()) {
-             
-            //$name = $request->get('name');
-            $name = "sofia"; //Here must be the value introduced through the body
-            Category::where('id', $id)->update(['name' => $name]);
-            $categories = Category::where('name', $name)->first();
-            
-            if ($categories != null) {
-                if ($name != $categories->name) {
-                    return response("This category already exists",400); exit;
+                    $user = self::getUserfromToken();
+                    $tokenDecoded = LoginController::decodeToken($headers['Authorization']);
+                    if ($tokenDecoded->password == $user->password and $tokenDecoded->email == $user->email) 
+                    {
+                        return true;
+                    }
                 }
             }
 
-            if (!ctype_graph($name)) {
-                return response("The name of the category can't have any blank space", 400); exit;
+            private function getUserfromToken()
+            {
+                $headers = getallheaders();
+                $tokenDecoded = LoginController::decodeToken($headers['Authorization']);
+                $user = User::where('email', $tokenDecoded->email)->first();
+                return $user;
+            }
+            /**
+             * Display a listing of the resource.
+             *
+             * @return \Illuminate\Http\Response
+             */
+            public function index() //shows all categories 
+            {
+                if (self::isLoggedIn())
+                {   
+                    $categories = Category::all();
+                    $categoryNames = [];
+                    $categoryIDs = [];
+
+                    if(empty($categories))
+                    {
+                        return response("There are no categories created",400);
+                    } 
+
+                    foreach ($categories as $category) {
+                        array_push($categoryNames, $category->name);
+                        array_push($categoryIDs, $category->id);
+                    } 
+
+                    return response()->json([
+                        'categories' => $categoryNames
+                        'ids' => $categoryIDs
+                    ],200);
+                    
+
+                } else {
+                    return response("You don't have permission", 403);
+                }
             }
 
-            if (empty($name)) {
-                return response("The name of the category is empty", 400); exit;
+            /**
+             * Show the form for creating a new resource.
+             *
+             * @return \Illuminate\Http\Response
+             */
+            public function create()
+            {
+
             }
 
-        } else {
-            return response("You don't have permission", 403);
-        }
-    }
+            /**
+             * Store a newly created resource in storage.
+             *
+             * @param  \Illuminate\Http\Request  $request
+             * @return \Illuminate\Http\Response
+             */
+            public function store(Request $request)
+            {
+                if (self::isLoggedIn())
+                {
+                    $name = $_POST['name'];
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        if (self::isLoggedIn()) {
-            $categoryToDelete = Category::find($id);
-            $categoryToDelete->delete();
-        }
+                    if (!ctype_graph($name)) {
+                        return response("The name of the category can't have any blank space", 400); exit;
+                    }
 
-    }
-}
+                    if (empty($name)) {
+                        return response("The name of the category is empty", 400); exit;
+                    }
+
+                    $category = Category::where('name', $name)->first();
+
+                    if ($category != null) {
+                        if ($name != $category->name) {
+                            return response("This category already exists",400); exit;
+                        }
+                    }
+                    
+
+                    $user_id = self::getUserfromToken()->id; //id del usuario del token;
+
+                    $category = new Category;
+
+                    $category->name = $name;
+                    $category->user_id = $user_id;
+
+                    $category->save();
+
+                } else {
+                    return response("You don't have permission", 403);
+                }
+            }
+
+            /**
+             * Display the specified resource.
+             *
+             * @param  \App\Category  $category
+             * @return \Illuminate\Http\Response
+             */
+            public function show(Category $category)
+            {
+
+            }
+
+            /**
+             * Show the form for editing the specified resource.
+             *
+             * @param  \App\Category  $category
+             * @return \Illuminate\Http\Response
+             */
+            public function edit(Category $category)
+            {
+                //
+            }
+
+            /**
+             * Update the specified resource in storage.
+             *
+             * @param  \Illuminate\Http\Request  $request
+             * @param  \App\Category  $category
+             * @return \Illuminate\Http\Response
+             */
+            public function update($id, Request $request)
+            {
+                if (self::isLoggedIn()) 
+                {
+
+                    //$name = $request->get('name');
+                    $name = "sofia"; //Here must be the value introduced through the body
+                    Category::where('id', $id)->update(['name' => $name]);
+                    $categories = Category::where('name', $name)->first();
+                    
+                    if ($categories != null)
+                    {
+                        if ($name != $categories->name)
+                        {
+                            return response("This category already exists",400); exit;
+                        }
+                    }
+
+                    if (!ctype_graph($name))
+                    {
+                        return response("The name of the category can't have any blank space", 400); exit;
+                    }
+
+                    if (empty($name))
+                    {
+                        return response("The name of the category is empty", 400); exit;
+                    }
+
+                } else {
+                    return response("You don't have permission", 403);
+                }
+            }
+
+            /**
+             * Remove the specified resource from storage.
+             *
+             * @param  \App\Category  $category
+             * @return \Illuminate\Http\Response
+             */
+            public function destroy($id)
+            {
+                if (self::isLoggedIn())
+                {
+                    $categoryToDelete = Category::find($id);
+                    $categoryToDelete->delete();
+                }
+
+            }
+        }
--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -1,56 +1,56 @@
-<?php
- 
-namespace App\Http\Controllers;
- 
-use Illuminate\Http\Request;
-use App\User;
- 
-class RegistrationController extends Controller
-{
-    const ID_ROL = 2;
-       
-    public function store()
+    <?php
+     
+    namespace App\Http\Controllers;
+     
+    use Illuminate\Http\Request;
+    use App\User;
+     
+    class RegistrationController extends Controller
     {
-        $name = $_POST['user'];
-        
-        if (!ctype_graph($name)) {
-            return response("The user name must be only one word", 400); exit;
-        }
-
-        $email = $_POST['email'];
-        
-        $user = User::where('email', $email)->first();
-
-        if($user != null){
-            if ($email == $user->email) {
-                return response("The email already exists",400); exit;
+        const ID_ROL = 2;
+           
+        public function store()
+        {
+            $name = $_POST['user'];
+            
+            if (!ctype_graph($name)) {
+                return response("The user name must be only one word", 400); exit;
             }
+
+            $email = $_POST['email'];
+            
+            $user = User::where('email', $email)->first();
+
+            if($user != null){
+                if ($email == $user->email) {
+                    return response("The email already exists",400); exit;
+                }
+            }
+
+            $password = $_POST['password'];
+            
+            if (strlen($password) < 8) {
+                return response("Invalid password. It must be at least 8 characters long.",400); exit;
+            }
+
+            $rol_id = self::ID_ROL;
+
+            $user = new User;
+
+            $user->name = $name;
+            $user->email = $email;
+            $encondedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $user->password = $encondedPassword;
+            $user->rol_id = $rol_id;
+
+            $user->save();
+            var_dump("User created");
         }
 
-        $password = $_POST['password'];
-        
-        if (strlen($password) < 8) {
-            return response("Invalid password. It must be at least 8 characters long.",400); exit;
+        public function destroy($id)
+        {
+            $user = User::find($id);
+            $user->delete();
+            var_dump("Your account has been deleted");
         }
-
-        $rol_id = self::ID_ROL;
-
-        $user = new User;
-
-        $user->name = $name;
-        $user->email = $email;
-        $encondedPassword = password_hash($password, PASSWORD_DEFAULT);
-        $user->password = $encondedPassword;
-        $user->rol_id = $rol_id;
-
-        $user->save();
-        var_dump("User created");
     }
-
-    public function destroy($id)
-    {
-        $user = User::find($id);
-        $user->delete();
-        var_dump("Your account has been deleted");
-    }
-}
